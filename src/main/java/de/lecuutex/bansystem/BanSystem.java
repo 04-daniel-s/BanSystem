28d54c0ee6d3e07b25bd2f8fcfa0443178ea10af
zYoru <xininfected@gmail.com>
2022-11-09 22:42:48 +0100

    Initial commit

---
+++ src/main/java/de/lecuutex/bansystem/BanSystem.java
@@ -0,0 +1,19 @@
+package de.lecuutex.bansystem;
+
+import lombok.Getter;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BanSystem extends Plugin {
+
+    @Getter
+    private static BanSystem instance;
+
+    @Override
+    public void onEnable() {
+        System.out.println("BanSystem is up an running");
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
